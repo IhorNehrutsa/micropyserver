599f78fd56ede0abd802618dfd85328a832e5364
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2020-12-22 15:12:44 +0200

    WIP: async MicroPyServer

--- amicropyserver.py
+++ amicropyserver.py
@@ -26,9 +26,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 import re
-import socket
 import sys
 import io
+import utime
+import gc
 import uasyncio as asyncio
 
 
@@ -47,82 +48,79 @@ class aMicroPyServer(object):
         
         self.swriter = None
 
+        self._counter = 0  # Remove it in the production release.
+
     async def start(self):
         """ Start server """
-        print('Awaiting client connection.')
+        print('Awaiting client connection on {}:{}'.format(self._host, self._port))
         self.server = await asyncio.start_server(self.run_client, self._host, self._port, self.backlog)
         while True:
-            await asyncio.sleep(100)
+            await asyncio.sleep(1)
 
     async def run_client(self, sreader, swriter):
-        print('Got connection from client', self, sreader, swriter)
-        #self.sreader = sreader
+        self.start_time = utime.ticks_ms()
+        print('Got connection from client', self, sreader, sreader.s.fileno(), sreader.e['peername'])
         self.swriter = swriter
-        '''
-        while True:
-            try:
-                self._connect, address = sock.accept()
-                request = self._get_request()
-                if len(request) == 0:
-                    self._connect.close()
-                    continue
-                if self._on_request_handler:
-                    if not self._on_request_handler(request, address):
-                        continue
-                route = self.find_route(request)
-                if route:
-                    route["handler"](request)
-                else:
-                    self.not_found()
-            except Exception as e:
-                    self.internal_error(e)
-            finally:
-                self._connect.close()
-        '''
         try:
-            while True:
-                try:
-                    res = await asyncio.wait_for(sreader.readline(), self.timeout)
-                except asyncio.TimeoutError:
-                    res = b''
-                if res == b'':
-                    raise OSError
-                print('Received {} from client {}'.format(res, 12345))
+            if sreader.s.fileno() != -1:
+                request = b''
+                while True:
+                    try:
+                        res = await asyncio.wait_for(sreader.readline(), self.timeout)
+                        request += res
+                        if res == b'\r\n':  # end of HTTP request
+                            break
+                    except asyncio.TimeoutError as e:
+                        print(1, e, "asyncio.TimeoutError", self.timeout)
+                        res = b''
+                    if res == b'':  # socket connection broken
+                        print('raise OSError')
+                        raise OSError
                 
-                request = str(res, "utf8")
-                try:
-                    route = self.find_route(request)
-                    print("route", route)
-                    if route:
-                        route["handler"](request)
-                    else:
-                        self.not_found()
-                except Exception as e:
-                    print(e)
-                    self.internal_error(e)
-                    raise
+                if request:
+                    request = str(request, "utf8")
+                    print('request >>>>{}<<<<'.format(request))
+                    
+                    try:
+                        route = self.find_route(request)
+                        if route:
+                            #print("route", route)
+                            await route["handler"](request)
+                        else:
+                            await self.not_found()
+                    except Exception as e:
+                        print(2, e)
+                        self.internal_error(e)
+                        raise
                         
-        except OSError:
+        except OSError as e:
+            print(3, e)
             pass
-        print('Client {} disconnect.'.format(self))
-        await sreader.wait_closed()
-        print('Client {} socket closed.'.format(self))
+        if swriter.s.fileno() != -1:
+            print('Client disconnect.', self, swriter, swriter.s.fileno(), swriter.e['peername'])
+            await swriter.wait_closed()
+            await asyncio.sleep(0.2)
+            print('Client socket closed.', self, swriter, swriter.s.fileno(), swriter.e['peername'])
+        print('Render time: %i ms' % utime.ticks_diff(utime.ticks_ms(), self.start_time))
+            
+        gc.collect()
+        print('----------------------------------------------------------------------------------')
 
-    def close(self):
-        print('Closing server')
+    async def close(self):
+        print('Closing server...')
         self.server.close()
         await self.server.wait_closed()
-        print('Server closed')
+        print('Server is closed.')
 
     def add_route(self, path, handler, method="GET"):
         """ Add new route  """
         self._routes.append({"path": path, "handler": handler, "method": method})
 
-    def _write(self, response):
+    async def _write(self, response):
+        print(response)
         self.swriter.write(response)
-        await self.swriter.drain() 
         
-    def send(self, response, status=200, content_type="Content-Type: text/plain", extra_headers=[]):
+    async def send(self, response, status=200, content_type="Content-Type: text/plain", extra_headers=[]):
         """ Send response to client """
 
         if self.swriter is None:
@@ -130,20 +128,30 @@ class aMicroPyServer(object):
 
         status_message = {200: "OK", 400: "Bad Request", 403: "Forbidden", 404: "Not Found",
                           500: "Internal Server Error"}
-        self._write("HTTP/1.0 " + str(status) + " " + status_message[status] + "\r\n")
-        self._write(content_type + "\r\n")
+        await self._write("HTTP/1.0 " + str(status) + " " + status_message[status] + "\r\n")
+        await self._write(content_type + "\r\n")
         for header in extra_headers:
-            self._write(header + "\r\n")
-        self._write("X-Powered-By: MicroPyServer\r\n")
-        self._write("\r\n")
-        self._write(response)
+            await self._write(header + "\r\n")
+        ### await self._write("X-Powered-By: MicroPyServer\r\n")  # not required, vainglory
+        await self._write("Cache-Control: no-store\r\n")  # The response may not be stored in any cache.
+                                                          # This is necessary to execute the code on the server:
+                                                          # switch PIN ON and switch PIN OFF.
+                                                          # This prevents showing the cashed text
+                                                          # when a user presses the "Backward/Forward" button in a browser.
+        await self._write("\r\n")  # end of HTTP header
+        self._counter += 1
+        await self._write(str(self._counter) + ' ' + response)
+        
+        #print("swriter.out_buf >>>", self.swriter.out_buf, '<<<')
+        await self.swriter.drain()
+        print("swriter.out_buf >>>", self.swriter.out_buf, '<<<')
+        print("Finished processing request.")
+        
         
     def find_route(self, request):
         """ Find route """
-        print('self._routes', self._routes)
-        print(type(request), '>>>', request, '<<<')
         lines = request.split("\r\n")
-        print('lines', lines)
+        #print('lines', lines)
         if len(lines[0]) > 0:
             method = re.search("^([A-Z]+)", lines[0]).group(1)
             for route in self._routes:
@@ -151,32 +159,28 @@ class aMicroPyServer(object):
                     continue
                 path = re.search("^[A-Z]+\\s+(/[-a-zA-Z0-9_.]*)", lines[0]).group(1)
                 if path == route["path"]:
+                    print('1', method, path, route["path"])
                     return route
                 else:
                     match = re.search("^" + route["path"] + "$", path)
                     if match:
-                        print(method, path, route["path"])
+                        print('2', method, path, route["path"])
                         return route
         return None
     
-    def not_found(self):
+    async def not_found(self):
         """ Not found action """
-        self.send("404", status=404)
+        await self.send("404", status=404)
 
     def internal_error(self, error):
         """ Catch error action """
         output = io.StringIO()
-        #sys.print_exception(error, output)
-        print(error)
+        sys.print_exception(error, output)
         str_error = output.getvalue()
         output.close()
-        self.send("Error: " + str_error, status=500)
+        if swriter.s.fileno() != -1:
+            self.send("Error: " + str_error, status=500)
 
     def on_request(self, handler):
         """ Set request handler """
         self._on_request_handler = handler
-
-    def _get_request(self):
-        """ Return request body """
-        return str(self._connect.recv(4096), "utf8")
-
--- examples/aexample1.py
+++ examples/aexample1.py
@@ -5,122 +5,52 @@ Needed ESP8266 or ESP32 board
 
 @see https://github.com/troublegum/micropyserver
 """
-import esp
 import network
-import usocket as socket
-import uasyncio as asyncio
-import time
-import machine
 from amicropyserver import aMicroPyServer
+from machine import Pin
+import uasyncio as asyncio
 
-wlan_id = "your wi-fi"
-wlan_pass = "your password"
-
-wlan_id = "MikroTik_mAP"
-wlan_pass = "1qazxsw2"
-
-# wlan_id = "telecard208"
-# wlan_pass = "208208208"
-
-# wlan_id = "TP-LINK-359734"
-# wlan_pass = "+380482359734"
+PIN_NUMBER = 2
+#wlan_id = "your wi-fi"
+#wlan_pass = "your password"
 
 wlan = network.WLAN(network.STA_IF)
-if wlan.isconnected():
-    print("  wlan.ifconfig():", wlan.ifconfig())
-    wlan.disconnect()
-    if not wlan.isconnected():
-        print('wlan disconnected')
-    
-#wlan.active(False)  # Comment out this line to throw an exception when wlan.ifconfig('dhcp') will execute 
-#wlan.config (dhcp_hostname = "espressif12345")
-wlan.active(True)
-try:
-    print("try: wlan.ifconfig('dhcp')", end=' ')
-    #wlan.ifconfig('dhcp')
-    print("- Ok")
-except Exception as e:
-    print("")
-    print(e)
-    print(e.args[0])
-    try:
-        print("EspError:", EspError.err_to_name(EspError(), 0x5004))
-    except:
-        print(" Approve pull request #6638, https://github.com/micropython/micropython/pull/6638")
-        print('  to see the "EspError: ESP_ERR_TCPIP_ADAPTER_DHCP_ALREADY_STARTED"')
-    print("")
-        
-while not wlan.isconnected():
-    wlan.connect(wlan_id, wlan_pass)
+#wlan.active(True)
+#if not wlan.isconnected():
+#    wlan.connect(wlan_id, wlan_pass)
 
-    print("  wlan.ifconfig():", wlan.ifconfig())
-    print("  wlan.status():", wlan.status())
-    
-    machine.idle()
-    time.sleep(1)
-
-print("")
 print("wlan.ifconfig():", wlan.ifconfig())
-print("wlan.status():", wlan.status())
 print("wlan.isconnected():", wlan.isconnected())
-print("wlan.config('mac')", wlan.config('mac'))
-print("wlan.config('essid')", wlan.config('essid'))
-try:
-    print("wlan.config('channel')", end=" ")
-    print(wlan.config('channel'))
-except Exception as e:
-    print(e)
-try:
-    print("wlan.config('hidden')", end=" ")
-    print(wlan.config('hidden'))
-except Exception as e:
-    print(e)
-try:
-    print("wlan.config('authmode')", end=" ")
-    print(wlan.config('authmode'))
-except Exception as e:
-    print(e)
-print("wlan.config('dhcp_hostname')", wlan.config('dhcp_hostname'))
-#wlan.config (dhcp_hostname = "espressif12345")
-#print("wlan.config('dhcp_hostname')", wlan.config('dhcp_hostname'))
 
-# print(socket.getaddrinfo(wlan.ifconfig()[0], 80)[0][-1])
-# print(socket.getaddrinfo('www.micropython.org', 80)[0][-1])
-# print(socket.getaddrinfo('micropython.org', 80)[0][-1])
-# print(socket.getaddrinfo('google.com', 80)[0][-1])
-# print(socket.getaddrinfo('127.0.0.1', 80)[0][-1])
-# try:
-#     #print("try: socket.getaddrinfo(wlan.config('dhcp_hostname'), 80)[0][-1]", end=' ')
-#     #print(socket.getaddrinfo(wlan.config('dhcp_hostname'), 80)[0][-1])
-#     print(socket.getaddrinfo('espressif.local', 80)[0][-1])
-#     #print("- Ok")
-# except Exception as e:
-#     print("")
-#     print(e)
-#     print(e.args[0])
-#     try:
-#         print("EspError:", EspError.err_to_name(EspError(), 202))
-#     except:
-#         print(" Approve pull request #6638, https://github.com/micropython/micropython/pull/6638")
-#         print('  to see the "EspError: ESP_ERR_TCPIP_ADAPTER_DHCP_ALREADY_STARTED"')
-#     print("")
+pin = Pin(PIN_NUMBER, Pin.OUT, value=0)
+
+async def show_index_page(request):
+    await server.send("THIS IS INDEX PAGE")
 
 
-def show_index_page(request):
-    server.send("THIS IS INDEX PAGE")
+async def show_info_page(request):
+    await server.send("THIS IS INFO PAGE")
 
 
-def show_info_page(request):
-    server.send("THIS IS INFO PAGE")
+async def show_pin_page(request):
+    if request.startswith("GET /on HTTP"):
+        pin.value(1)
+    elif request.startswith("GET /off HTTP"):
+        pin.value(0)
+    await server.send("PIN IS " + ("ON" if pin.value() == 1 else "OFF"))
 
 
-server = aMicroPyServer()
+server = aMicroPyServer(host=wlan.ifconfig()[0], backlog=1)
 server.add_route("/info", show_info_page)
 server.add_route("/", show_index_page)
+server.add_route("/on", show_pin_page)
+server.add_route("/off", show_pin_page)
+
 
 try:
     asyncio.run(server.start())
 except KeyboardInterrupt:
     print('Interrupted')  # This mechanism doesn't work on Unix build.
 finally:
+    asyncio.run(server.close())
     _ = asyncio.new_event_loop()
